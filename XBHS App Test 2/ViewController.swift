8a73b3d8d9b688f4dae96d4aaf869f13a27fae01
Griffin Phenegar <GriffinPhenegar@Patricks-iMac.local>
2015-03-18 16:26:30 -0400

    Initial Commit

---
+++ XBHS App Test 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XBHS App Test 2
+//
+//  Created by Griffin Phenegar on 3/18/15.
+//  Copyright (c) 2015 Griffin Phenegar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
